a2c2fcc7c898a60ac2d4b7a06b75e520f515300e
Meow <abodkaramma77@gmail.com>
2025-01-25 22:55:49 +0200

    new update solved some problems

--- engine.py
+++ engine.py
@@ -4,10 +4,18 @@ print("Welcome In Rock_Paper_Scisors Game!")
 firstChoose = getpass('First player choose: ')
 secondChoose = getpass('Second player choose: ')
 def check(first,second):
- searchFirstInput=re.search("^r|s|p",first,re.I)
- searchSecondInput=re.search("^r|s|p",second,re.I)
- firstResult=searchFirstInput.group().lower()
- secondResult=searchSecondInput.group().lower()
+ if first and second : 
+  checkTheResult(dataBase()[0],dataBase()[1])
+ else:
+   print("sorry but you must add a game choice ")  
+def dataBase():
+ searchFirstInput=re.search("^r|s|p",firstChoose,re.I)
+ searchSecondInput=re.search("^r|s|p",secondChoose,re.I)
+ return searchFirstInput,searchSecondInput
+
+def checkTheResult(first,second):
+ firstResult=first.group().lower()
+ secondResult=second.group().lower()
  print("The First Player Choosed {} \nThe Second Player Choosed {}".format(firstResult,secondResult))
  if firstResult==secondResult:
   print("draw!!")
@@ -23,5 +31,4 @@ def check(first,second):
    print(f"""The First Player Won!!""")
  if firstResult=="p" and secondResult=="s" :
    print(f"""The Second Player Won!!""")
-
-check(firstChoose,secondChoose)
+check(dataBase()[0],dataBase()[1])
